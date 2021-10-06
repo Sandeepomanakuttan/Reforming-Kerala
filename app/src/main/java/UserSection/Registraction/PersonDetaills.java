08d327409055c9a1bc83543d9e677634a79c3b7f
SandeepOmanakuttan <sandeepomanakuttan04@gmail.com>
2021-10-06 12:22:56 +0530

    adding user section

---
+++ app/src/main/java/UserSection/Registraction/PersonDetaills.java
@@ -0,0 +1,14 @@
+package UserSection.Registraction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonDetaills extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_detaills);
+    }
+}
