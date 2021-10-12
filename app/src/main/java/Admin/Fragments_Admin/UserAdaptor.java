3f0991560376c84ac1d6cddd5b66c4bde83ac73a
SandeepOmanakuttan <90667098+SandeepOmanakuttan@users.noreply.github.com>
2021-10-13 00:21:27 +0530

    first commit

---
+++ app/src/main/java/Admin/Fragments_Admin/UserAdaptor.java
@@ -0,0 +1,12 @@
+package Admin.Fragments_Admin;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+import Admin.PersonDataCollection;
+
+public class UserAdaptor {
+    public UserAdaptor(Context context, ArrayList<PersonDataCollection> collections) {
+    }
+}
---
+++ app/src/main/java/Admin/Fragments_Admin/view_Adaptor.java
@@ -0,0 +1,12 @@
+package Admin.Fragments_Admin;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+import Admin.datacollectionClass.ProfileData;
+
+public class view_Adaptor {
+    public view_Adaptor(Context context, ArrayList<ProfileData> collections) {
+    }
+}
